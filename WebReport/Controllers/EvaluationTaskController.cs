9899f0c853c0791d456d6dea79004b97852ca783
Adelvig <adelvig@yandex.ru>
2022-10-25 22:47:29 +0700

    Реализация контролера и представления для EvaluationTask

---
+++ WebReport/Controllers/EvaluationTaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebReport.Controllers
+{
+    public class EvaluationTaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
